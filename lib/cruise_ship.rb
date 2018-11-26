a513e93acdd5c52b97db8a9447e9acb7188f5cfc
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 00:12:02 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,8 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  passengers.each { |k,v| if k == :suite_a && v.start_with?('A') return v }
+  passengers.each do |k,v| 
+    if k == :suite_a && v.start_with?('A') 
+      return v 
+    end
 end
